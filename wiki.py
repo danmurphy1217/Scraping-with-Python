e194627016b840df07bf1e4cd4e1e387a3f0fa3b
Dan Murphy <danielmurph8@gmail.com>
2019-10-06 23:01:00 -0400

    Initial connection & scrape

--- wiki.py
+++ wiki.py
@@ -1,1 +1,12 @@
+import html 
+import requests
+from bs4 import BeautifulSoup
 
+response = requests.get("https://en.wikipedia.org/wiki/Main_Page")
+print(response) #check for successful request
+bs = BeautifulSoup(response.text, "html.parser")
+spanTags = bs.findAll('span')
+num = 1
+for tag in spanTags:
+    print("This is tag " + str(num) + ":" + str(tag) + "\n")
+    num += 1
